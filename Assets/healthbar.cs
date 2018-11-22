7dac86caeb13289e933f7ee32fda7ad00936ea39
DESKTOP-GNA0N9Q\JUN <solbon1212@naver.com>
2018-11-22 15:41:45 +0900

    health bar

---
+++ Assets/healthbar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthbar : MonoBehaviour {
+
+    private Transform bar;
+
+	private void Start (){
+        bar = transform.Find("Bar:);
+    }
+	
+    public void Setsize(float sizeNormalized)
+    {
+        bar.localScale = new.Vector3(sizeNormalized, 1f);
+            
+    }
+	void Update () {
+		
+	}
+}
---
+++ Assets/smokeAttack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utills;
+
+public class smokeAttack : MonoBehaviour {
+
+    [SerializeField] private healthbar healthbar;
+
+    void Start () {
+        healthbar.Setsize(.4f);
+           Function functio
+    }
+	
+}
